037a2a9b57d2fd2a932a628b8dd8d9bec280e4a4
Tarocco <tarocco.bun@hotmail.com>
2016-06-09 16:46:05 -0700

    LayerAttribute Lets you mark a layer integer so that the inspector uses EditorGUI.LayerField Why is this not built in to Unity???

---
+++ Source/RoaringFangs/Editor/LayerPropertyDrawer.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+using RoaringFangs.Attributes;
+
+namespace RoaringFangs.Editor
+{
+    using UnityEngine;
+    using UnityEditor;
+
+    [CustomPropertyDrawer(typeof(LayerAttribute))]
+    public class LayerDrawer : PropertyDrawer
+    {
+        public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+        {
+            property.intValue = EditorGUI.LayerField(position, label, property.intValue);
+        }
+    }
+}
