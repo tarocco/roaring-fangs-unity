197502298047036880f4acd6b0a37f5a1cf8bbbe
Tarocco <bun@taroc.co>
2017-03-03 00:09:12 -0800

    Configurators, refactors, misc.

---
+++ Source/Third Party/BetterEventSystem.cs
@@ -0,0 +1,28 @@
+﻿/*
+Adapted from EventSystem improvement by Lyuben Atanasov
+http://blog.pastelstudios.com/2015/09/07/unity-tips-tricks-multiple-event-systems-single-scene-unity-5-1/
+*/
+
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
+
+public class BetterEventSystem : EventSystem
+{
+    protected override void OnEnable()
+    {
+        // Do not assign EventSystem.current
+    }
+
+    protected override void Update()
+    {
+        if (gameObject.scene == SceneManager.GetActiveScene())
+        {
+            EventSystem original = current;
+            // In order to avoid reimplementing half of the EventSystem class, just
+            // temporarily assign this EventSystem to be the globally current one
+            current = this;
+            base.Update();
+            current = original;
+        }
+    }
+}
